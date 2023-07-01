131df0831548014006ecefabc045a419c062179e
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-01 22:34:10 +0000

    added create tables to schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,62 @@
 -- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS quizs CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS results CASCADE;
+DROP TABLE IF EXISTS taker_answers CASCADE;
+DROP TABLE IF EXISTS results CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  date_created TIMESTAMP
+);
+
+CREATE TABLE quizs (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  date_created TIMESTAMP
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizs(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE taker_answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  result_id INTEGER REFERENCES results(id) ON DELETE CASCADE,
+  option VARCHAR(255) NOT NULL,
+  is_correct BOOLEAN,
+  answer_id INTEGER REFERENCES answers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE taker_answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  result_id INTEGER REFERENCES results(id) ON DELETE CASCADE,
+  option VARCHAR(255) NOT NULL,
+  is_correct BOOLEAN,
+  answer_id INTEGER REFERENCES answers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_id INTEGER REFERENCES quizs(id) ON DELETE CASCADE,
+  favourited BOOLEAN,
+  best_score TEXT,
+  current_score TEXT,
+  rating INTEGER,
+  attempt INTEGER
 );

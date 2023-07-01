eedaeee5b530b6c1cb013ccbc531743f0019d6d5
dburnham1212 <dburnham1212@gmail.com>
2023-07-01 18:30:55 -0400

    Adding ability to login and logout

--- routes/myQuizzes.js
+++ routes/myQuizzes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
+const { users, getUserByEmail } = require("../database_placeholders/users");
+
 router.get('/', (req, res) => {
-  res.render('myQuizzes');
+  const userID = req.session.userID;
+
+  const templateVars = {user: users[userID]};
+  res.render('myQuizzes', templateVars);
 });
 
 module.exports = router;

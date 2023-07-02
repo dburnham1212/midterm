776d61a56a9db05785415d910e12eae1ad958a05
dburnham1212 <dburnham1212@gmail.com>
2023-07-02 13:52:58 -0400

    Removing unused login route

--- routes/submitQuiz.js
+++ routes/submitQuiz.js
@@ -7,17 +7,23 @@ router.get('/:id', (req, res) => {
   const userID = req.session.userID;
   console.log(req.params.id);
   let currentQuiz;
-  for(const quiz in quizzes){
+  for(const quiz of quizzes){
     if(quiz.id === req.params.id){
       currentQuiz=quiz;
     }
   }
-
+  console.log(req.body);
+  for(const question of questions){
+    if(currentQuiz.id === question.quiz_id){
+      console.log(req.body[question.quiz_id]);
+    }
+  }
   const templateVars = {user: users[userID], quiz: currentQuiz };
   res.render('submitQuiz', templateVars);
 });
 
 router.post('/:id', (req, res) => {
+  console.log(req.body);
   res.redirect(`/submitQuiz/${req.params.id}`);
 });
 

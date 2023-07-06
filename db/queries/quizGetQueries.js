c454b69f3e8fc31a2cd98f202e057bd2ea9f8746
dburnham1212 <dburnham1212@gmail.com>
2023-07-06 10:55:30 -0400

    Fixing syncing issues

--- db/queries/quizGetQueries.js
+++ db/queries/quizGetQueries.js
@@ -93,4 +93,18 @@ const getQuizByTitleAndUserID = (title, userID) => {
   });
 };
 
-module.exports = { getQuizzesByPublic, getMyQuizzesByID, getFavQuizzesByUserId, getQuizAvgRatingById, getQuizByQuizId, getQuizByTitleAndUserID }
+const getTitleByUser = function(quiz) {
+  const questString = `
+  SELECT title FROM quizs
+  WHERE user_id = $1
+  AND title = $2
+  `;
+  const container = [`${quiz.user_id}`, `${quiz.title}`];
+  return db.query(questString, container)
+  .then(res => {
+    return res.rows;
+  });
+}
+
+
+module.exports = { getQuizzesByPublic, getMyQuizzesByID, getFavQuizzesByUserId, getQuizAvgRatingById, getQuizByQuizId, getQuizByTitleAndUserID, getTitleByUser }

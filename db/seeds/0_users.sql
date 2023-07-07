871f364af72d3544b341f57d1d575c6d64710877
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-07 13:38:52 +0000

    fixed database

--- db/seeds/0_users.sql
+++ db/seeds/0_users.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (username, email, password)
+VALUES ('QuizardWizard', 'gandalfthegr8@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
+INSERT INTO users (username, email, password)
+VALUES ('DinoDan', 'dinolover123@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
+INSERT INTO users (username, email, password)
+VALUES ('Shania1556', 'twainpain18@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
+INSERT INTO users (username, email, password)
+VALUES ('sherlockandkey', 'arthurconan55@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
+INSERT INTO users (username, email, password)
 VALUES ('userID', 'test@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
-INSERT INTO users (username, email, password)
-VALUES ('userID2', 'test2@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
-INSERT INTO users (username, email, password)
-VALUES ('userID3', 'test3@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
-INSERT INTO users (username, email, password)
-VALUES ('userID4', 'test4@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
-INSERT INTO users (username, email, password)
-VALUES ('userID5', 'test5@gmail.com', '$2a$10$xiMLnOMFA.V6DPvPu.x5fegvmdbwVuR1vapQhLuMCQhRA3PJOZiKq');
--- db/seeds/2_quizs.sql
+++ db/seeds/2_quizs.sql
@@ -1,20 +1,20 @@
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (1, '1f. Do you know Animals?', 4, false );
+VALUES (1, 'Do you know Animals?', 4, false );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (1, '1t. Cartoon Trivia', 4, true );
+VALUES (1, 'Cartoon Trivia', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (2, '2t. Chemistry Trivia', 4, true );
+VALUES (2, 'Chemistry Trivia', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (2, '2t. Geography Quiz', 4, true );
+VALUES (2, 'Geography Quiz', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (3, '3t. Christmas Trivia', 4, true );
+VALUES (3, 'Christmas Trivia', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (3, '3t. Easy History', 4, true );
+VALUES (3, 'Easy History', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (4, '4t. Dinosaurs', 4, true );
+VALUES (4, 'Dinosaurs', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (4, '4t. Biology', 4, true );
+VALUES (4, 'Biology', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (5, '5t. Nursery Rhymes', 4, true );
+VALUES (5, 'Nursery Rhymes', 4, true );
 INSERT INTO quizs (user_id, title, rating, public)
-VALUES (5, '5t. Disney Trivia', 4, true );
+VALUES (5, 'Disney Trivia', 4, true );

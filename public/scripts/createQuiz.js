a56387ad5464aa2dcc66dd8983dc6cb64b26fe67
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-07 02:45:24 +0000

    css styling

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -1,34 +1,50 @@
 $(() => {
   let questionCount = 0;
   const addquestionTemplate = function () {
-    const $quizquestion = (`<div class="question-card" name="question-card">
-    <div class="question-container">
+    const $quizquestion = (`
+<section class="question-main">
+<article class="question-card" name="question-card">
+
+  <div class="question-container">
     <label>Question:</label>
     <textarea name="${questionCount}" required></textarea>
     <br>
   </div>
-  <div class="answer-container">
-    <label class = "answer-text">Answer 1:</label>
+</article>
+
+<article class="answer-card">
+<div class="answer-container">
+  <div class="answer">
+    <label class = "answer-text">Answer 1: </label>
     <input name="input${questionCount}" class = "answer-input-field" required>
     <input class="answer-checkbox" name="answer${questionCount}" value=1 type = "radio" required>
   </div>
-  <div class="answer-container">
-    <label class = "answer-text">Answer 2:</label>
+
+  <div class="answer">
+    <label class = "answer-text">Answer 2: </label>
     <input name="input${questionCount}" class = "answer-input-field" required>
     <input class="answer-checkbox" name="answer${questionCount}" value=2 type = "radio" required>
   </div>
-  <div class="answer-container">
-    <label class = "answer-text">Answer 3:</label>
+
+  <div class="answer">
+    <label class = "answer-text">Answer 3: </label>
     <input name="input${questionCount}" class = "answer-input-field" required>
     <input class="answer-checkbox" name="answer${questionCount}" value=3 type = "radio" required>
   </div>
-  <div class="answer-container">
-    <label class = "answer-text">Answer 4:</label>
+
+  <div class="answer">
+    <label class = "answer-text">Answer 4: </label>
     <input name="input${questionCount}" class = "answer-input-field" required>
     <input class="answer-checkbox" name="answer${questionCount}" value=4 type = "radio" required>
   </div>
-  <button class="danger-button delete-question-button">Delete question</button>
-  </div>`)
+</article>
+
+<article class="delete-card">
+<div class="delete-container">
+    <button class="danger-button delete-question-button">Delete question</button>
+</div>
+</article>
+</section>`)
 
     return $quizquestion
   }
